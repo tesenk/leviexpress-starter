3e6515a9ad157735e7186c48c18a688f4e20c5ee
Tereza Senkyrova <senkyrovatt@gmail.com>
2021-05-29 10:29:18 +0200

    router created

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,22 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Reservation } from '../Reservation';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export const App = () => (
   <>
-    <Header />
-    <Home />
-    <Footer />
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/reservation">
+          <Reservation />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   </>
 );
---
+++ src/components/Reservation/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Reservation = () => (
+  <>
+    <h2>Zobraz rezervaci</h2>
+  </>
+);
